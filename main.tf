161d9a82646e2bbc5adcdfc77bc38f0bf55c1fea
RadhaKrishna-2020 <radhakrishna.pawankumar@gmail.com>
2025-09-15 20:03:33 +0530

    Update and rename EC2 to main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+provide "aws" {
+region = "us-east-1"
+Access_key = "AKIAXQVNZIZLFBYQLKHV"
+secret_key = "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"
+}
+
+resource "aws_instance" "myec2" {
+   ami = "ami-0b09ffb6d8b58ca91"
+   instance_type = "t2.micro"
+}
